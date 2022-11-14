f7a92248eed3d198efb35249b33834cf68cb926b
Willy Laurent Brenkman <88841329+LaurentBrenkman@users.noreply.github.com>
2022-11-14 11:57:39 +0700

    Sign in and Sign Up Solved

---
+++ lib/ui/pages/bonus_pages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class BonusPage extends StatelessWidget {
+  const BonusPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
